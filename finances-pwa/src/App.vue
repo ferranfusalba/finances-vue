f7f1ae71aceea9150663ddd721f9eb393ee0c3ac
ferranfusalba <52327422+ferranfusalba@users.noreply.github.com>
2022-05-28 12:17:29 +0200

    dev-pwa: cloned, adapted

--- finances-pwa/src/App.vue
+++ finances-pwa/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -28,5 +28,41 @@
       color: #42b983;
     }
   }
+}
+</style>
+ -->
+
+ <template>
+  <!-- <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Form msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Form
+  }
+}
+</script>
+
+<style>
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- finances-pwa/src/components/HelloWorld.vue
+++ finances-pwa/src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
@@ -43,7 +43,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
@@ -59,4 +58,62 @@ li {
 a {
   color: #42b983;
 }
+</style> -->
+
+<template>
+  <div class="sample">
+    <h1>Example use of @carbon/vue</h1>
+    <cv-text-input
+      label="Who are you?"
+      v-model="yourName"
+      placeholder="your name"
+    />
+    <cv-button @click="onClick">Hello {{yourName}}</cv-button>
+    <cv-modal :visible="visible" @modal-hidden="modalClosed">
+      <template slot="title">Welcome to @carbon/vue {{yourName}}</template>
+      <template slot="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, seed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </template>
+    </cv-modal>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        yourName: '',
+        visible: false,
+      };
+    },
+    methods: {
+      onClick() {
+        this.visible = true;
+      },
+      modalClosed() {
+        this.visible = false;
+      },
+    },
+  };
+</script>
+
+<style>
+  .sample {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 600px;
+    margin: 5% auto;
+  }
+
+  .cv-text-input {
+    margin: 30px 0;
+  }
 </style>
