00834a5e410cd17f1f2573eb8adb579bc6d16e43
ferranfusalba <52327422+ferranfusalba@users.noreply.github.com>
2022-05-28 22:27:12 +0200

    applying text selection prevention on tab bar

--- finances-pwa/src/components/TabBarSection.vue
+++ finances-pwa/src/components/TabBarSection.vue
@@ -24,6 +24,13 @@ a {
     align-items: center;
     justify-content: space-evenly;
     height: 100%;
+    * {
+      user-select: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+    }
     p {
       font-size: 11px;
     }
