05a76c914032e202209571f0189af65c48702bec
ferranfusalba <52327422+ferranfusalba@users.noreply.github.com>
2022-06-24 21:23:23 +0200

    testing in-house sausage menu

---
+++ finances-pwa/src/components/ButtonSausage.vue
@@ -0,0 +1,42 @@
+<template>
+  <li class="nav-item bg-white border border-dark rounded p0">
+    <a class="nav-link active text-dark pLR"
+      ><router-link class="tdn" :to="pageURL">{{ pageName }}</router-link></a
+    >
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ButtonSausage",
+  props: ["pageURL", "pageName"],
+};
+</script>
+
+<style>
+li {
+  margin-right: 6px;
+}
+.p0 {
+  padding-left: 0;
+  padding-right: 0;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.pLR {
+  padding-left: 12px;
+  padding-right: 12px;
+}
+.tdn {
+  text-decoration: none;
+  color: black;
+}
+.tdn:hover {
+  text-decoration: none;
+  color: black;
+}
+</style>
--- finances-pwa/src/components/HorizontalScrollAccounts.vue
+++ finances-pwa/src/components/HorizontalScrollAccounts.vue
@@ -1,10 +1,10 @@
 <template>
-  <Flicking :options="{ moveType: 'freeScroll', bound: true }">
-    <!-- <div v-for="item in this.$store.state.accounts" :key="item">
+  <!-- <Flicking :options="{ moveType: 'freeScroll', bound: true }">
+    <div v-for="item in this.$store.state.accounts" :key="item">
         <cv-button :icon="Currency20" @click="goTo(item.route)">
           {{ item.name }}
         </cv-button>
-      </div> -->
+      </div>
     <span class="button mr-2 is-white">🍎 Apple</span>
     <span class="button mr-2 is-white">🍉 Watermelon</span>
     <span class="button mr-2 is-white">🥝 Kiwi</span>
@@ -14,19 +14,48 @@
     <span class="button mr-2 is-white">🍎 Apple</span>
     <span class="button mr-2 is-white">🍉 Watermelon</span>
     <span class="button mr-2 is-white">🥝 Kiwi</span>
-  </Flicking>
+  </Flicking> -->
+  <!-- <div>
+    <ul
+      class="nav bg-white row flex-row flex-nowrap mb-4"
+      style="overflow-x: auto"
+    >
+    <ButtonSausage pageURL="/work/calendas" pageName="Calendas"/>
+    <ButtonSausage pageURL="/work/elearning" pageName="eLearning Platform"/>
+    <ButtonSausage pageURL="/work/notorious" pageName="Notorious"/>
+    <ButtonSausage pageURL="/work/shine" pageName="Shine"/>
+    <ButtonSausage pageURL="/work/ui" pageName="UI Design"/>
+    <ButtonSausage pageURL="/work/fds" pageName="Florí"/>
+    <ButtonSausage pageURL="/work/other" pageName="Other Projects"/>
+    </ul>
+  </div> -->
+  <nav class="sausage-links">
+    <ul>
+        <li><a href="#">Room Service</a></li>
+        <li><a href="#">Hotel Services</a></li>
+        <li><a href="#">Info</a></li>
+        <li><a href="#">TV Channels</a></li>
+        <li><a href="#">Discover Barcelona</a></li>
+        <li><a href="#">Museum Tour</a></li>
+        <li><a href="#">Room Lights</a></li>
+        <li><a href="#">Chat with Reception</a></li>
+        <li><a href="#">Call Reception</a></li>
+    </ul>
+</nav>
 </template>
 
 <script>
-import { Flicking } from "@egjs/vue-flicking";
-import "@egjs/vue-flicking/dist/flicking.css";
-import "@egjs/vue-flicking/dist/flicking-inline.css";
+// import { Flicking } from "@egjs/vue-flicking";
+// import "@egjs/vue-flicking/dist/flicking.css";
+// import "@egjs/vue-flicking/dist/flicking-inline.css";
 import Currency20 from "@carbon/icons-vue/lib/currency/20";
 import router from "../router";
+// import ButtonSausage from "@/components/ButtonSausage.vue";
 
 export default {
   components: {
-    Flicking: Flicking,
+    // Flicking: Flicking,
+    // ButtonSausage
   },
   computed: {
     Currency20() {
@@ -40,3 +69,78 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss"> /*
+.row > * {
+  width: auto;
+}
+.row {
+  padding-left: 12px;
+}
+ul {
+  -webkit-overflow-scrolling: touch;
+}
+ul::-webkit-scrollbar {
+  display: none;
+} */
+
+main {
+    background: linear-gradient(90deg, #81d0e8 -20.27%, #8ddce8 100%);
+    border-radius: 0px;
+    box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+    margin: 2em auto;
+    max-width: 375px;
+    padding: 8px 12px 8px 12px;
+    width: 100%;
+}
+
+menu {
+    background: linear-gradient(90deg, #009ED6 -20.27%, #1BBAC7 100%);
+    border-radius: 0px;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.15);
+    margin: 2em auto;
+    max-width: 375px;
+    padding: 17px 21px 17px 21px;
+    width: 100%;
+}
+
+.sausage-links {
+    position: relative;
+    
+    &:before, &:after {
+        content: '';
+        height: calc(100% - 2em);
+        pointer-events: none;
+        position: absolute;
+        top: 1em;
+        width: 10px;
+        z-index: 2;
+    }
+
+    ul {
+        display: flex;
+        flex-wrap: nowrap;
+        overflow: auto;
+        -webkit-overflow-scrolling: touch;
+        
+        li {
+            white-space: nowrap;
+            
+            a,a:visited {
+                border: 1px;
+                border-radius: 4px;
+                // color: white;
+                display: inline-block;
+                padding: 4px 8px;
+                text-decoration: none;
+                
+                &:hover {
+                    background: white;
+                    color: #04A2D5;
+                    border-color: white;
+                }
+            }
+        }
+    }
+}
+</style>
